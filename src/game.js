28f671d512a294f75113c67fbf84f2fd31996c80
Максим <bogdanov.mv.95@gmail.com>
2025-02-26 13:12:42 +0600

    fix scale
    (cherry picked from commit 16b65fdaaa877231154f617b5437777bcd345a71)
    
    (cherry picked from commit ab1b142ea2051e4b4000d3ce08b20e399a9838f2)

--- src/game.js
+++ src/game.js
@@ -13,8 +13,11 @@ const config = {
         CharacterSelectionScene,
         GameScene
     ],
-    parent: 'gameContainer',
     pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 
     color: {
         smoke: 0xDDDDDD,
